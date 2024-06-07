3b22668e676300e7637a330398d92cbc509ab496
Bastien <bastien.youssfi@gmail.com>
2024-06-07 11:28:43 +0900

    Fare

--- Neural Network/main.py
+++ Neural Network/main.py
@@ -1,6 +1,10 @@
 import os
 from pathlib import Path
-import torch, numpy as np, pandas as pd
+import matplotlib
+matplotlib.use('Agg')  # Change the backend to 'Agg' or another compatible backend
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch, numpy as np
 
 # Environment variable
 iskaggle = os.environ.get('KAGGLE_KERNEL_RUN_TYPE', '')
@@ -15,8 +19,26 @@ else:
         kaggle.api.competition_download_cli(str(path))
         zipfile.ZipFile(f'{path}.zip').extractall(path)
 
+# Display options
 np.set_printoptions(linewidth=140)
 torch.set_printoptions(linewidth=140, sci_mode=False, edgeitems=7)
 pd.set_option('display.width', 140)
 
+# Load the data
 df = pd.read_csv(path/'train.csv')
+
+# Gets the amount of NaN values in the dataset
+naNum = df.isna().sum()
+
+# mode calculates the most frequent value in the dataset for each column, iloc[0] gets the first row
+modes = df.mode().iloc[0]
+
+# Fills the NaN values with the most frequent value
+df.fillna(modes, inplace=True)
+
+# Generates summary only for the numerical columns
+describe = df.describe(include=(np.number))
+
+df['Fare'].hist()
+
+print()
